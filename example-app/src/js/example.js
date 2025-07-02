35d0a9647aacbbc5b805fcdd897c421175c072b2
Stanislav Mayorov <stanislavmayorav@gmail.com>
2025-07-02 16:02:47 +0300

    chore: fix prettier/link errors

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,6 +1,6 @@
 import { AdaptyCapacitorPlugin } from '@adapty/capacitor';
 
 window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    AdaptyCapacitorPlugin.echo({ value: inputValue })
-}
+  const inputValue = document.getElementById('echoInput').value;
+  AdaptyCapacitorPlugin.echo({ value: inputValue });
+};
