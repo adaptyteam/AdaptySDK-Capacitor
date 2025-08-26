4bbbe9d3752de5749b6a515b43e0ffb5a6e7f6a3
Stanislav Mayorov <stanislavmayorav@gmail.com>
2025-08-26 20:20:00 +0300

    feat: add Adapty to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,21 +3,25 @@ import PackageDescription
 
 let package = Package(
     name: "AdaptyCapacitor",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "AdaptyCapacitor",
             targets: ["AdaptyCapacitorPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/adaptyteam/AdaptySDK-iOS.git", .exact("3.10.1"))
     ],
     targets: [
         .target(
             name: "AdaptyCapacitorPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "Adapty", package: "AdaptySDK-iOS"),
+                .product(name: "AdaptyUI", package: "AdaptySDK-iOS"),
+                .product(name: "AdaptyPlugin", package: "AdaptySDK-iOS")
             ],
             path: "ios/Sources/AdaptyCapacitorPlugin"),
         .testTarget(
