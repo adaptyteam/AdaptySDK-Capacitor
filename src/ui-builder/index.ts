2cdbe01b86775d7318ff4b42cf58890a3df934d5
Stanislav Mayorov <stanislavmayorav@gmail.com>
2026-01-16 19:29:25 +0300

    feat(devtools): add export for AdaptyCustomAsset type

--- src/ui-builder/index.ts
+++ src/ui-builder/index.ts
@@ -3,6 +3,7 @@ export { PaywallViewController } from './paywall-view-controller';
 export { OnboardingViewController } from './onboarding-view-controller';
 export type { CreatePaywallViewParamsInput, CreateOnboardingViewParamsInput } from './types';
 export type {
+  AdaptyCustomAsset,
   AdaptyUiView,
   AdaptyUiDialogConfig,
   AdaptyUiDialogActionType,
