a0a7cdcd2d36566aebda9fd2949feb3450ad4638
Stanislav Mayorov <stanislavmayorav@gmail.com>
2026-02-26 08:54:18 -0800

    refactor: replace local method/api/schema name types with @adapty/core re-exports

--- src/shared/types/schema.d.ts
+++ src/shared/types/schema.d.ts
@@ -1,4 +1,4 @@
-import type { components } from './api';
+import type { components } from '@adapty/core';
 export type Req = components['requests'];
 export type Def = components['defs'];
 export type Event = components['events'];
